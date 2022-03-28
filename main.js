c2a39de55a31366300811b730d7713e1ad353919
anjana29 <80512586+anjana29@users.noreply.github.com>
2022-03-28 18:59:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftwristx="";
+leftwristy="";
+rightwristy="";
+rightwristx="";
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -9,7 +13,25 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modeLoaded);
+    poseNet.on('pose',gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
 }
+function modeLoaded(){
+    console.log("PoseNet Is intialized");
+}
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    leftwristx=results[0].pose.leftWrist.x;
+    leftwristy=results[0].pose.leftWrist.y;
+    console.log("leftwristx = "+leftwristx+",leftwristy = "+leftwristy);
+    rightwristx=results[0].pose.rightWrist.x;
+    rightwristy=results[0].pose.rightWrist.y;
+    console.log("rightwristx = "+rightwristx+",rightwristy = "+rightwristy);
+
+}
+}
+
